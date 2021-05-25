72eae83c4df92ca074c126ef30452d955c4cfb21
Lala Sabathil <pkmoras.facebook@web.de>
2021-05-25 13:50:09 +0200

    add basic

---
+++ DSharpPlus.Examples/DSharpPlus.Examples.Bots/Program.cs
@@ -0,0 +1,17 @@
+﻿using DSharpPlus.Examples.Bots.Main;
+using System;
+using System.Reflection;
+
+namespace DSharpPlus.Examples.Bots
+{
+    class Program
+    {
+        static void Main(string[] args = null)
+        {
+            string dev = "ODIyMjQyNDQ0MDcwMDkyODYw.YFPa8w.bqrtuN3t7L1FQ9kRsm08clbJDUo";
+            Console.WriteLine($"{Assembly.GetExecutingAssembly().FullName.Split(",")[0]} has the permission to run. Yay!");
+            using var b = new Bot(dev);
+            b.RunAsync().Wait();
+        }
+    }
+}
