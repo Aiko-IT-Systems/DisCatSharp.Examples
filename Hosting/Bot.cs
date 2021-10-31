c7eb14005b7e4eae19d5f13fc89cd2a82fe4e192
Alice <quantuchi@pm.me>
2021-10-31 07:51:43 +0200

    Update Hosting example

--- Hosting/Bot.cs
+++ Hosting/Bot.cs
@@ -4,14 +4,15 @@ using DisCatSharp.CommandsNext;
 using DisCatSharp.Examples.Hosting.Commands;
 using DisCatSharp.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace DisCatSharp.Examples.Hosting
 {
     public class FirstBot : DiscordHostedService
     {
-        public FirstBot(IConfiguration config, ILogger<DiscordHostedService> logger, IServiceProvider provider) : base(
-            config, logger, provider, "FirstBot")
+        public FirstBot(IConfiguration config, ILogger<DiscordHostedService> logger, IServiceProvider provider, 
+            IHostApplicationLifetime appLifetime) : base(config, logger, provider, appLifetime, "FirstBot")
         {
             // You can do whatever you want here, for example logs or registering commands
             
@@ -22,8 +23,8 @@ namespace DisCatSharp.Examples.Hosting
 
     public class SecondBot : DiscordHostedService
     {
-        public SecondBot(IConfiguration config, ILogger<DiscordHostedService> logger, IServiceProvider provider) : base(
-            config, logger, provider, "SecondBot")
+        public SecondBot(IConfiguration config, ILogger<DiscordHostedService> logger, IServiceProvider provider,
+            IHostApplicationLifetime appLifetime) : base(config, logger, provider, appLifetime, "SecondBot")
         {
             // You can also configure all extensions here
             
@@ -32,6 +33,14 @@ namespace DisCatSharp.Examples.Hosting
             var ext = Client.UseApplicationCommands();
             
             ext.RegisterCommands<AppCommands>();
+        }
+
+        protected override void OnInitializationError(Exception ex)
+        {
+            Logger.LogError(ex, "An error occurred during the initialization of the second bot");
+            
+            // By default, the app will be shutdown, but we can override this method and do whatever we want here.
+            // Or just make the bot optional without shutting down the application, as in this example.
         }
     }
 }
